35c74cd56c222dd8bc94856e7db0190b30789389
Ranem97 <haffarranem@gmail.com>
2024-07-15 17:33:01 +0400

    added basic java script

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,3 +1,6 @@
-fetch("travel_recommendation_api.json")
-  .then((Response) => Response.json())
-  .then((data) => console.log(data.beaches[0]));
+// fetch("travel_recommendation_api.json")
+//   .then((Response) => Response.json())
+//   .then((data) => console.log(data.beaches[0]));
+const btnSearch = document.getElementById("btnSearch");
+const btnClear = document.getElementById("btnClear");
+const recommwndations = [];
